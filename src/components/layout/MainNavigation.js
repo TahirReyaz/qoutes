d26984280ddca24ba3267d085c3c68c6d008500d
TahirReyaz <tahir200213@st.jmi.ac.in>
2022-05-22 11:14:14 +0530

    Fixed api error

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -4,7 +4,7 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>Mah quotes</div>
+      <div className={classes.logo}>My quotes</div>
       <nav className={classes.nav}>
         <ul>
           <li>
